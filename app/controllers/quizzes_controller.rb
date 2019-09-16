a2c5d41a473154347c85fa76d9d81cc11d846914
Paula Tulis <tulis.paula@gmail.com>
2019-09-16 17:21:38 -0400

    going to need to re-structure models

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,10 +8,24 @@ class QuizzesController < ApplicationController
         @quiz = Quiz.find(params[:id])
     end 
 
+    def create
+        @quiz = Quiz.new(quiz_params)
+
+        if @quiz.save
+            render json: @quiz, each_serializer: QuizSerializer.new(@quiz), status: :accepted
+        else 
+            render json: {errors: ['did not save']}, status: :unprocessable_entity
+        end 
+    end 
+
     def destroy
         @quiz = Quiz.find(params[:id])
         @quiz.destroy
         redirect_to '/home', :notice => "Your quiz has been deleted"
+    end 
+private
+    def quiz_params
+        params.require(:quiz).permit(:title, :description, :score, :user_id, :subject_id)
     end 
 
 end
