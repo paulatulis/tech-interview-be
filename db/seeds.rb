97fde457acdc9f2685dd376c72fba2035bb4df8d
Paula Tulis <tulis.paula@gmail.com>
2019-10-11 15:34:28 -0400

    JS questiosn

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,19 +55,27 @@ a4rubyq4 = Answer.create(answer_body: "b = a", correct: false, quiz_question_id:
 
 
 
-
-reactq1 = QuizQuestion.create(question_body: 'What is hoisting?', quiz_id: quiz2.id)
-a1reactq1 = Answer.create(answer_body: "test", correct: false, quiz_question_id: reactq1.id)
-reactq2 = QuizQuestion.create(question_body: 'Here is another sample question', quiz_id: quiz2.id)
-reactq3 = QuizQuestion.create(question_body: 'Here is one more sample question', quiz_id: quiz2.id)
-
-jsq1 = QuizQuestion.create(question_body: 'What is hoisting?', quiz_id: quiz3.id)
-jsq2 = QuizQuestion.create(question_body: 'Here is another sample question', quiz_id: quiz3.id)
-jsq3 = QuizQuestion.create(question_body: 'Here is one more sample question', quiz_id: quiz3.id)
-
-# Answers 
+#JavaScript Quiz
+jsq1 = QuizQuestion.create(question_body: 'What is hoisting?', quiz_id: quiz1.id)
+a1jsq1 = Answer.create(answer_body: "variables and function declarations are moved to the bottom of their scope before code execution", correct: false, quiz_question_id: rubyq1.id)
+a2jsq1 = Answer.create(answer_body: 'variables and function declarations are moved to the top of whatever function they are declared in', correct: false, quiz_question_id: rubyq1.id)
+a3jsq1 = Answer.create(answer_body: 'variables and function declarations are moved to the top of their scope before code execution', correct: true, quiz_question_id: rubyq1.id)
+a4jsq1 = Answer.create(answer_body: "variables must be declared at the top of a function for code to execute", correct: false, quiz_question_id: rubyq1.id)
 
 
+jsq2 = QuizQuestion.create(question_body: "What's the difference between an undeclared and undefined variable?", quiz_id: quiz1.id)
+a1jsq2 = Answer.create(answer_body: "Undeclared variables don't exist, while undefined variables are exist but don't have a value.", correct: true, quiz_question_id: rubyq2.id)
+a2jsq2 = Answer.create(answer_body: "Undeclared variables don't hold a value, while undefined variables do not exist", correct: false, quiz_question_id: rubyq2.id)
+a3jsq2 = Answer.create(answer_body: "No difference; they are interchangeable", correct: false, quiz_question_id: rubyq2.id)
 
-# User Answer Choices
-# uac1 = UserAnswerChoice.create(letter: 'c', user_id: paula.id)
+rubyq3 = QuizQuestion.create(question_body: "What kind of language is JavaScript", quiz_id: quiz1.id)
+a1rubyq3 = Answer.create(answer_body: "strongly typed", correct: false, quiz_question_id: rubyq3.id)
+a2rubyq3 = Answer.create(answer_body: "server side scripting language", correct: false, quiz_question_id: rubyq3.id)
+a3rubyq3 = Answer.create(answer_body: "client side scripting language", correct: true, quiz_question_id: rubyq3.id)
+a4rubyq3 = Answer.create(answer_body: "none of the above", correct: false, quiz_question_id: rubyq3.id)
+
+rubyq4 = QuizQuestion.create(question_body: "What is a block statement?", quiz_id: quiz1.id)
+a1rubyq4 = Answer.create(answer_body: "a way to group statements", correct: true, quiz_question_id: rubyq4.id)
+a2rubyq4 = Answer.create(answer_body: "a way to define a variable", correct: false, quiz_question_id: rubyq4.id)
+a3rubyq4 = Answer.create(answer_body: "a method used to block off private functions", correct: false, quiz_question_id: rubyq4.id)
+a4rubyq4 = Answer.create(answer_body: "all of the above", correct: false, quiz_question_id: rubyq4.id)
